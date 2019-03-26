d9bdda98e0512ada52381611b6786e512d521865
Sol-Yh <48500467+Sol-Yh@users.noreply.github.com>
2019-03-26 10:11:33 +0900

    Update 03 26 C.c

--- C/03 26 C.c
+++ C/03 26 C.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+void test01(){
+	int a,b;
+	printf("첫번째 수 입력 : "); scanf("%d",&a);
+	printf("두번째 수 입력 : "); scanf("%d",&b);
+	printf("목 : %d  |  나머지 : %d\n",a/b,a%b);
+}
 main(){
-	
+	test01();
 }

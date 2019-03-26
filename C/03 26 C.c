44e9c374be132a15ec7d5e321872ca180d46bef8
Sol-Yh <48500467+Sol-Yh@users.noreply.github.com>
2019-03-26 10:14:09 +0900

    Update 03 26 C.c

--- C/03 26 C.c
+++ C/03 26 C.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 void test01(){
-	int a,b;
-	printf("첫번째 수 입력 : "); scanf("%d",&a);
-	printf("두번째 수 입력 : "); scanf("%d",&b);
-	printf("목 : %d  |  나머지 : %d\n",a/b,a%b);
+	int a,b;    					//정수형 변수 a,b 선언
+	printf("첫번째 수 입력 : "); scanf_s("%d",&a);   //a값 받기
+	printf("두번째 수 입력 : "); scanf_s("%d",&b);   //b값 받기
+	printf("목 : %d  |  나머지 : %d\n",a/b,a%b);     // a/b = a÷b    ||   a%b = a÷b의 나머지
 }
 main(){
 	test01();

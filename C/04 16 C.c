4bf164279a4acf1f6af94aa75473a09ec7694dd7
Sol-Yh <48500467+Sol-Yh@users.noreply.github.com>
2019-04-16 11:33:06 +0900

    Update 04 16 C.c

--- C/04 16 C.c
+++ C/04 16 C.c
@@ -39,16 +39,21 @@ void c041602(){
 }
 
 void c041603(){
-	int n = 100;
+	//초기화
+	int n = 100; //몇까지할껀데
 	int vlu = 0;
 	int i;
+	//초기화
+
+	printf("===================1~100중 짝수의 합 (for)\n");
 	for(i=1; i<=n; i++){
-		if(i%2==0){		// i%2==1  = 홀수의 합
+		if(i%2==0){		// i%2==1  = 홀수
 				vlu += i;
 				printf("%d\n",vlu);
 		}
 	}
-	printf("↑for===================↓while\n");
+	printf("===================1~100중 짝수의 합 (while)\n");
+
 	vlu=0;
 	i=1;
 	while(i<=n){
@@ -58,7 +63,49 @@ void c041603(){
 		}
 		i++;
 	}
+	printf("===================2의 배수, 3의 배수 (for)\n");
+	vlu=0;
+	i=1;
+	for(i=1; i<=n; i++){
+		if(i%2==0 || i%3==0){	//i%2==0 && i%3==0  =  2의 배수이면서 3의 배수인것만
+				vlu += i;
+				printf("%d || %d\n",i,vlu);
+		}
+	}
+	printf("===================2의 배수, 3의 배수 (while)\n");
+	vlu=0;
+	i=1;
+	while(i<=n){
+		if(i%2==0 || i%3==0){	//i%2==0 && i%3==0  =  2의 배수이면서 3의 배수인것만
+				vlu += i;
+				printf("%d || %d\n",i,vlu);
+		}
+		i++;
+	}
+}
+
+void c041604(){
+	int x,y;
+	printf("첫번째 수 입력 : "); scanf("%d",&x);
+	printf("두번째 수 입력 : "); scanf("%d",&y);
+	if(x%y==0)	
+		printf("두번째 수(%d)는 첫번째 수(%d)의 약수입니다\n",y,x);
+	else 
+		printf("두번째 수(%d)는 첫번째 수(%d)의 약수가 아닙니다\n",y,x);
+}
+
+void c041605(){
+	int x;
+	int i;
+	printf("약수구할 수 입력 : "); scanf("%d",&x);
+	printf("==%d의 약수==\n",x);
+	for(i=1; i<=x; i++){
+		if(x%i==0){
+			printf("%d\n",i);
+		}
+	}
+	
 }
 main(){
-	c041603();
+	c041605();
 }
